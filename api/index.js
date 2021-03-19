811cda71e8b50d6307966fc8bb5ff878ff339f64
ninew.jck <ninew.jck@gmai.com>
2021-03-19 19:53:43 +0700

    add

--- api/index.js
+++ api/index.js
@@ -9,9 +9,13 @@ fs.writeFileSync("/tmp/db.json", JSON.stringify(db()));
 let router = jsonServer.router("/tmp/db.json");
 let middlewares = jsonServer.defaults();
 let port = process.env.PORT || 5000;
-
 server.use(middlewares);
 server.use(router);
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 server.listen(port, function () {
   console.log("JSON Server is running on http://localhost:" + port);
 });
