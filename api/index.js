d35dda3a7ed19f189eefae69efbd627bd4f3067a
ninew.jck <ninew.jck@gmai.com>
2021-03-19 20:17:31 +0700

    add

--- api/index.js
+++ api/index.js
@@ -11,12 +11,21 @@ let middlewares = jsonServer.defaults();
 let port = process.env.PORT || 5000;
 server.use(middlewares);
 server.use(router);
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+server.options("*", cors());
+
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 server.listen(port, function () {
   console.log("JSON Server is running on http://localhost:" + port);
 });
