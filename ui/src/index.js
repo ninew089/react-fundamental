f6e4e6c591619312af881e1c193ef6c6ee34fb3d
ninew.jck <ninew.jck@gmai.com>
2021-03-19 18:03:03 +0700

    remove gitignore

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-axios.defaults.baseURL = 'https://react-fundamental-api.vercel.app/'
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
 ReactDOM.render(
   <React.StrictMode>
